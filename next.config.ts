3778110b7f9e68d3a526541dc657dbd419a2f571
Luke Sullivan <luke@sukelully.dev>
2025-08-21 12:44:29 +0100

    chore: set up prisma

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["http://127.0.0.1:3000"],
+      // @ts-ignore
+      allowedDevOrigins: ["http://127.0.0.1:3000"],
+    },
+  },
 };
 
 export default nextConfig;
