1065b7288f8bd8899fdb4fd74be72e079f2aa3dc
Luke Sullivan <luke@sukelully.dev>
2025-08-22 12:32:29 +0100

    remove spotify sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-4xl font-bold mb-4">Hueify</h1>
-      <button
+      {/* <button
         className="bg-green-500 text-white px-6 py-3 rounded-lg cursor-pointer"
         onClick={signIn}
       >
         Sign in with Spotify
-      </button>
+      </button> */}
     </div>
   );
 }
