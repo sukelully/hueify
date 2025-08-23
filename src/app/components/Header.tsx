b1d5d1fd4ef413a7d255419d151780a5abeb1f5c
Luke Sullivan <luke@sukelully.dev>
2025-08-23 17:03:33 +0100

    style: small update to btn active styling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="absolute top-0 left-0 z-10 flex w-full items-center justify-between gap-4 px-6 py-3 sm:px-32">
+    <header className="absolute top-0 left-0 z-10 flex w-full items-center justify-between gap-4 px-6 py-3 sm:px-12">
       <Link href="/">
         <h1 className="font-corben scale-x-110 scale-y-90 transform text-3xl font-bold tracking-widest sm:text-4xl">
           <span className="inline-block bg-gradient-to-r from-blue-600 via-fuchsia-400 to-rose-500 bg-clip-text text-transparent">
@@ -18,7 +18,7 @@ export default function Header() {
       {/* Desktop menu */}
       <nav className="hidden space-x-6 md:flex">
         <button
-          className="sign-in-btn hover:bg-black-hover dark:hover:bg-white-hover cursor-pointer rounded-lg bg-black px-3 py-1 font-semibold text-white dark:bg-white dark:text-black"
+          className="sign-in-btn hover:bg-black-active active:bg-black-active dark:active:bg-white-active dark:hover:bg-white-active cursor-pointer rounded-lg bg-black px-3 py-1 font-semibold text-white dark:bg-white dark:text-black"
           onClick={signIn}
         >
           Sign in
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col items-center justify-center px-4 sm:px-16">
       <button
         onClick={signIn}
-        className="sign-in-btn hover:bg-black-hover dark:hover:bg-white-hover text-md flex min-w-[180px] items-center gap-2 bg-black px-6 py-3 text-white sm:gap-3 sm:text-lg dark:bg-white dark:text-black"
+        className="sign-in-btn hover:bg-black-active active:bg-black-active dark:active:bg-white-active dark:hover:bg-white-active text-md flex min-w-[180px] items-center gap-2 bg-black px-6 py-3 text-white sm:gap-3 sm:text-lg dark:bg-white dark:text-black"
       >
         <Image src="/spotify/spotify-green.png" alt="Spotify logo" width={24} height={24} />
         <span>Sign in with Spotify</span>
