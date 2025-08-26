639fd27404720c8ffc0b91eaa974e37bd91531da
Luke Sullivan <luke@sukelully.dev>
2025-08-26 09:27:25 +0100

    rename redirect uri and trusted origins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,12 +6,12 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
-  trustedOrigins: ['http://127.0.0.1:3000'],
+  trustedOrigins: ['http://127.0.0.1:3000', 'https://hueify.vercel.app'],
   socialProviders: {
     spotify: {
       clientId: process.env.SPOTIFY_CLIENT_ID as string,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
-      redirectURI: 'http://127.0.0.1:3000/api/auth/callback/spotify',
+      redirectURI: 'http://hueify.vercel.app/api/auth/callback/spotify',
     },
   },
   session: {
