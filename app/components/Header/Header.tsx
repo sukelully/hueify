d3d3a19766e4be6ae92283a7d46d61cd3b9dfb84
Luke Sullivan <luke@sukelully.dev>
2025-08-28 10:43:36 +0100

    style: add test logo

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,16 +2,20 @@
 
 import Link from 'next/link';
 import HeaderDropdown from './HeaderDropdown';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header className="from-header-grad to-background fixed top-0 left-0 z-10 flex w-full items-center justify-between gap-4 bg-gradient-to-b px-6 py-3 lg:px-48">
-      <Link href="/" className="relative bottom-[4px]">
-        <h1 className="font-corben scale-x-110 scale-y-90 transform text-3xl font-bold tracking-widest md:text-4xl">
-          hueify
-        </h1>
-        <div className="from-grad-1 via-grad-2 to-grad-3 relative top-[3px] -z-10 h-[3px] rounded-lg bg-gradient-to-r"></div>
-      </Link>
+      <div className='flex flex-row gap-4'>
+        {/* <Image src="/header/logo3.png" alt="Hueify logo" width={32} height={32} className='object-contain'/> */}
+        <Link href="/" className="relative bottom-[5px]">
+          <h1 className="font-corben scale-x-110 scale-y-90 transform text-3xl font-bold tracking-widest md:text-4xl">
+            hueify
+          </h1>
+          <div className="from-grad-1 via-grad-2 to-grad-3 relative top-[3px] -z-10 h-[3px] rounded-lg bg-gradient-to-r"></div>
+        </Link>
+      </div>
 
       <nav className="flex items-center gap-4 space-x-6">
         <Link href="/about" className="desktop-header-item hidden md:block">
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="min-w-[280px] md:scroll-smooth">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${corben.variable} antialiased`}
       >
